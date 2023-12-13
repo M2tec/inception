8d05430e09c91e12c91f31f99fb787530e73254a
Maarten Menheere <m.menheere@m2tec.nl>
2023-12-13 12:30:29 +0100

    Search bar update

--- src/components/SearchAppBar.js
+++ src/components/SearchAppBar.js
@@ -1,32 +1,31 @@
 import * as React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { List } from 'react-bootstrap-icons';
+import { List, Search } from 'react-bootstrap-icons';
 import DarkMode from './DarkMode/DarkMode'
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 export default function SearchAppBar() {
   return (
-    <div className='navbar-x'>
-    <Navbar>
-        <Nav.Link><List size="20px"/></Nav.Link>
-        <Navbar.Brand href="#home">GC Playground</Navbar.Brand>
-        <div class="container">
-    <div class="row">
-        <div class="col-12">
-            <div class="input-group">
-                <input class="form-control border-secondary py-2" type="search" value="search"/>
-                <div class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="button">
-                        <i class="fa fa-search"></i>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    <div>
+      <Navbar>
+        <Nav.Link className='m-3'><List size="20px" /></Nav.Link>
+        <Navbar.Brand className='nav-brand' href="#home">GC Playground</Navbar.Brand>
 
+        <InputGroup className="ms-5 me-5">
+          <InputGroup.Text id="basic-addon3">
+            Local: Token locking
+          </InputGroup.Text>
+          <Form.Control id="basic-url"
+                    aria-label="Small"
+                    aria-describedby="inputGroup-sizing-sm" />
+        </InputGroup>
+
+        <div className='me-3'>
         <DarkMode/>
-    </Navbar>
+        </div>
+      </Navbar>
     </div>
   );
 }

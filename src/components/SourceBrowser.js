8d621627390369f323f7592d493a9d9fca88dced
Maarten Menheere <m.menheere@m2tec.nl>
2023-12-21 14:59:34 +0100

    Add file dialog box

--- src/components/SourceBrowser.js
+++ src/components/SourceBrowser.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { ChevronDown, FiletypeJson } from 'react-bootstrap-icons';
 import { AppContext } from '../AppContext';
+import { Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
 export default function SourceBrowser() {
     // const [activeItem, setActiveItem] = React.useState(0);
     const { context, setContext } = React.useContext(AppContext)
-    
+    const [addFile, setAddFile] = React.useState(false);
+
     const File = ({
         item,
         isActive,
@@ -23,6 +26,21 @@ export default function SourceBrowser() {
             </div>
         );
     };
+    
+    function handleCreateFile() {
+        console.log("hi")
+        setAddFile(true)
+    }
+
+    function handleAddFile() {
+        console.log("hi")
+        setAddFile(true)
+    }
+
+    function handleCancelAddFile() {
+        console.log("hi")
+        setAddFile(false)
+    }
 
     return (
         <div className="source-browser">
@@ -41,11 +59,20 @@ export default function SourceBrowser() {
                             let filename = oldContext.items[index].name
                             openFiles.indexOf(filename) === -1 ? openFiles.push(filename) : console.log("This item already exists");
                             // console.log(filename)
-                            return { ...oldContext, active: filename, openFiles: openFiles}
+                            return { ...oldContext, active: filename, openFiles: openFiles }
                         })}
                     />
                 );
             })}
+            {addFile ?
+                <Form>
+                    <Form.Control className="ms-5 mt-2 w-50" placeholder="Enter filename..." />
+                    <Button onClick={handleCreateFile} variant="success" className="rounded ms-5 mt-2">Add file</Button>
+                    <Button onClick={handleCancelAddFile} variant="secondary" className="rounded mt-2 ms-1">Cancel</Button>
+                </Form>
+                :
+                <Button onClick={handleAddFile} variant="primary" className="rounded ms-5 mt-2">+</Button>}
+
         </div>
     );
 }

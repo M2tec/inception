966245fd877da952026e5840bc2a53ff56c8b203
Maarten Menheere <m.menheere@m2tec.nl>
2023-12-24 08:11:59 +0100

    Improve menu item css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import GcSideBar from './GcSideBar';
 import SearchAppBar from './SearchAppBar';
 import DataView from "./DataView";
 import {
@@ -17,13 +16,10 @@ export default function Home() {
     const { context, setContext } = React.useContext(AppContext)
     const isActiveAGCScript = context.active && context.active.endsWith('.gcscript');
 
-    const [menuActive, setMenuActive] = React.useState("home");
+    // const [menuActive, setMenuActive] = React.useState("home");
     const [type, setType] = React.useState("source")
 
     function handleClickHome(e) {
-        // let url = window.location.origin;
-        // console.log(url)
-        // window.location.replace(url)
         setType("source")
     }
 
@@ -57,14 +53,9 @@ export default function Home() {
 
         if (window.focus) { newwindow.focus() }
         return false;
-
     }
 
     function handleClickData(e) {
-        // let url = window.location.origin + window.location.pathname + "return-data";
-        // console.log(url)
-        // let testUrl = "http://localhost:3000/return-data/1-H4sIAAAAAAAAA11QTU8rMQz8Lzn3kDhfm94Q7_CQEFxAQkJPyGtnW2iz201SaLfqf3_bInHAt5mxx_acRDzshlyLWJ7E_UCbtz8xDRewncHz08ujWMrFFYjl60nshu07He9YLAUyioXAUmJ9wBR_mHGPfX2vx5mw8lri_G8hSsJcb4e-ZqR60Zy1Ul1kALBaa-u1cS4qYnaNVKpFllIrExrbKPHL4C-W9WxCMHdYhQ4btLbF2EHUrCW4IOcxNEDWxpZbz8rIiByVR6vlb7sb5hxLuf7A-a3GUtWUd-MhrBxDCPVrLQP5fuxsKZtwDBr8epNwKg0kqg4-A49fH7DX_JFxJVPsI1AKcbu3icdEU5gI8rQaPdmVG-2BepPFd7BPh3lvG6h10rCi-UqF3RwCBSM1YTRKgTatCrEjT-QbUiSdxM4Fb1zHAJpAnM_n_6NIvcTNAQAA"
-        // window.location.replace(url)
         setType("returndata")
     }
 
@@ -81,10 +72,33 @@ export default function Home() {
             <div className="View">
 
                 <div className='GcSideBar'>
-                    <Button onClick={handleClickHome} variant="primary"><Files size={"20px"} /></Button>
-                    <Button onClick={handleClickRun} disabled={!isActiveAGCScript} variant="primary"><PlayFill size={"20px"} /></Button>
-                    <Button onClick={handleClickData} variant="primary"><ArrowReturnLeft size={"20px"} /></Button>
-                    <Button onClick={handleClickPopup} variant="primary"><CloudUploadFill size={"20px"} /></Button>
+                    <Button 
+                        className={type === "source" ? "btn-primary btn-active" : "btn-primary"}
+                        onClick={handleClickHome} 
+                        variant="primary">
+                            <Files size={"20px"} />
+                    </Button>
+
+                    <Button 
+                        onClick={handleClickRun} 
+                        disabled={!isActiveAGCScript} 
+                        variant="primary">
+                            <PlayFill size={"20px"} />
+                    </Button>
+                    
+                    <Button 
+                        className={type === "returndata" ? "btn-primary btn-active" : "btn-primary"}
+                        onClick={handleClickData} 
+                        variant="primary">
+                            <ArrowReturnLeft size={"20px"} />
+                    </Button>
+
+                    <Button 
+                        onClick={handleClickPopup} 
+                        variant="primary">
+                            <CloudUploadFill size={"20px"} />
+                    </Button>
+
                 </div>
                 {/* <GcSideBar /> */}
                 <DataView type={type} />

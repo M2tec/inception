8c2a3b42fa2e5dfd06271d45938948f1f04c3836
Maarten Menheere <m.menheere@m2tec.nl>
2023-12-26 11:47:05 +0100

    Fix RightContext removal import 2

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ export default function App() {
         </Route>
 
         <Route path="testpopup" element={<TestPopup />} />
-        <Route path="testcontextmenu" element={<RightContext />} />
         <Route path="*" element={<NoMatch />} />
 
       </Routes>
